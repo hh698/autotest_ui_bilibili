ed4e5dbf1c09987d6c41a217000877fa80f1f3ac
hh698 <1277490394@qq.com>
2024-09-17 14:35:30 +0800

    submit0917
    下一次任务：
    简单重构用例，把参数的传递直接放到用例中，复用性更高
    
    记录：
    图灵api对于文字坐标的获取可能存在问题，目前还没有很好的解决方法，暂时只能靠运气实现定位

--- autotest_ui_bilibili/TestCase/test_login.py
+++ autotest_ui_bilibili/TestCase/test_login.py
@@ -24,14 +24,23 @@ class TestChannel:
             bp = BilibiliLoginPage(refresh_web)  # 传递refresh_web得到的driver
             page_title = bp.login_process()
         with allure.step("步骤2：断言"):
-            assert page_title == "登录验证"
-            # assert page_title == "哔哩哔哩 (゜-゜)つロ 干杯~-bilibili"
+            # assert page_title == "登录验证"
+            assert page_title == "哔哩哔哩 (゜-゜)つロ 干杯~-bilibili"
 
+
+"""
+新增一个错误的账号密码登录的用例，思路：
+去掉bilibili_login_page中的login_process，直接调用webstar方法，传入username和password参数
+这个过程在测试用例里面完成，让测试用例直接控制传参
+
+对webstar方法进行修改，其中直接调用pass_touclick方法
+
+同时对第一个用例也进行修改
+"""
 
 # if __name__ == '__main__':
 #     pytest.main(["-s", "-v", "test_login.py"])
 #     os.system("allure generate Outputs/reports/allure-results -o Outputs/reports/allure-report --clean")
-    # pytest.main(
-    #     [["-s", "-v", "-m", "sj", "--html=Outputs/report/pytest.html", "--alluredir=Outputs/reports/allure-results"],
-    #      "test_login.py"])
-
+# pytest.main(
+#     [["-s", "-v", "-m", "sj", "--html=Outputs/report/pytest.html", "--alluredir=Outputs/reports/allure-results"],
+#      "test_login.py"])
